75106b57c230a7048ddd90fe69bb740cac9e6805
Monica <monicatoribio@bcp.com.pe>
2025-03-10 11:48:21 -0500

    feat: update timeout

--- app/src/main/java/com/mvvm/composenavigation/networking/RetrofitInstance.kt
+++ app/src/main/java/com/mvvm/composenavigation/networking/RetrofitInstance.kt
@@ -4,12 +4,16 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
     private const val BASE_URL = "https://backend4app.onrender.com/api/v1/"
 
     private val client by lazy {
         OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS) // Tiempo de espera de conexión
+            .readTimeout(60, TimeUnit.SECONDS)    // Tiempo de espera de lectura
+            .writeTimeout(60, TimeUnit.SECONDS)   // Tiempo de espera de escritura
             .addInterceptor(
                 HttpLoggingInterceptor().apply {
                     level = HttpLoggingInterceptor.Level.BODY
