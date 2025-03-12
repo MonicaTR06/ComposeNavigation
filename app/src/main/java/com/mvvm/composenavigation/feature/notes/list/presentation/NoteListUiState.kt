fba5362d4c84df3cc660ee9ab8acaa44dc8b5a8a
Monica <monicatoribio@bcp.com.pe>
2025-03-12 16:20:31 -0500

    feat: convert to module

---
+++ app/src/main/java/com/mvvm/composenavigation/feature/notes/list/presentation/NoteListUiState.kt
@@ -0,0 +1,9 @@
+package com.mvvm.composenavigation.feature.notes.list.presentation
+
+import com.mvvm.composenavigation.feature.notes.create.data.response.NoteResponse
+
+data class NoteListUiState(
+    val isLoading: Boolean = false,
+    val displayErrorMessage: Boolean = false,
+    val notes: List<NoteResponse> = arrayListOf(),
+)
